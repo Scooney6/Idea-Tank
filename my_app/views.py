8a17dabcca416f16d57b24c8b56aa65c537b65cf
Scooney6 <66892347+Scooney6@users.noreply.github.com>
2020-12-14 23:21:37 -0500

    home page setup

--- my_app/views.py
+++ my_app/views.py
@@ -2,6 +2,14 @@ from my_app import app
 from flask import render_template, request, redirect
 import requests
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    return redirect("/home")
+
+
+@app.route("/home", methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        player_name = request.form["player_name"]
+        session_code = request.form["session_code"]
+    return render_template("home.html")
