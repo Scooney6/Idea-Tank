b319878bc38055436ec45c416d95143e56993c36
Scooney6 <66892347+Scooney6@users.noreply.github.com>
2020-12-15 18:25:52 -0500

    add basic create session page

--- my_app/views.py
+++ my_app/views.py
@@ -2,6 +2,7 @@ from my_app import app
 from flask import render_template, request, redirect
 import requests
 
+
 @app.route("/", methods=["POST", "GET"])
 def index():
     return redirect("/home")
@@ -10,6 +11,17 @@ def index():
 @app.route("/home", methods=["POST", "GET"])
 def home():
     if request.method == "POST":
-        player_name = request.form["player_name"]
-        session_code = request.form["session_code"]
-    return render_template("home.html")
+        if request.form.get("Join"):
+            player_name = request.form["player_name"]
+            session_code = request.form["session_code"]
+        elif request.form.get("Create"):
+            return redirect("/create")
+        else:
+            return render_template("home.html")
+    else:
+        return render_template("home.html")
+
+
+@app.route("/create", methods=["POST", "GET"])
+def create():
+    return render_template("create.html")
