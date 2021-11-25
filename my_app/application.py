e29d5978f7e0c1b07d699a548bafc77c374bf4d4
Scooney6 <66892347+Scooney6@users.noreply.github.com>
2021-11-25 01:09:34 -0500

    Fix player leaves not updating. Implement Leader reasignment if the leader leaves.

--- my_app/application.py
+++ my_app/application.py
@@ -1,7 +1,7 @@
 from random import randint
 
 from flask import Flask, render_template, request, redirect, url_for
-from flask_socketio import SocketIO, send, join_room, leave_room, emit
+from flask_socketio import SocketIO, join_room, leave_room, emit
 from my_app.WTForms import *
 import sqlite3 as sql
 
@@ -19,11 +19,11 @@ def index():
     return redirect("/home")
 
 
+# home route renders the home join or create form
 @app.route("/home", methods=["POST", "GET"])
 def home():
     # instantiate the form
     home_join_form = HomeJoinForm(request.form)
-
     if request.method == "POST":
         # if the create form is submitted
         if request.form.get("Create"):
@@ -32,9 +32,10 @@ def home():
         elif home_join_form.validate():
             code = home_join_form.join_code.data
             username = home_join_form.username.data
+            # enter the user to the db as not a leader
             with sql.connect("rooms.db") as con:
                 cur = con.cursor()
-                cur.execute("INSERT INTO rooms (username, room) VALUES (?, ?)", (username, code))
+                cur.execute("INSERT INTO rooms (username, room, isleader) VALUES (?, ?, ?)", (username, code, 0))
             return redirect(url_for("lobby", code=code, username=username))
         # otherwise render the home page again with errors if necessary
         else:
@@ -44,68 +45,90 @@ def home():
         return render_template("home.html", form=home_join_form)
 
 
+# create route renders create lobby form
 @app.route("/create", methods=["POST", "GET"])
 def create():
     # instantiate create form
     create_form = CreateForm()
-
     # if the create form is submitted and valid
     if create_form.validate_on_submit():
         code = create_code()
         username = create_form.username.data
+        # add the user the the db as the leader of this room
         with sql.connect("rooms.db") as con:
             cur = con.cursor()
-            cur.execute("INSERT INTO rooms (username, room) VALUES (?, ?)", (username, code))
+            cur.execute("INSERT INTO rooms (username, room, isleader) VALUES (?, ?, ?)", (username, code, 1))
         return redirect(url_for("lobby", code=code, username=username))
     # otherwise render the template with instantiated form and errors if necessary
     else:
         return render_template("create.html", form=create_form)
 
 
+# render the lobby with the code, everything else is taken care of with js
 @app.route("/lobby", methods=["POST", "GET"])
 def lobby():
     cd = request.args.get('code')
     return render_template("lobby.html", code=cd)
 
 
+# Event bucket for when the client connects
 @socketio.on('join')
 def on_join(data):
     username = data['username']
     room = data['room']
+    # put the user in their socketio room
     join_room(room)
+    # get the current users and leader from the db
     with sql.connect("rooms.db") as con:
         cur = con.cursor()
         cur.execute("SELECT username FROM rooms WHERE room = (?)", (room,))
         users = cur.fetchall()
-    # Sends to everyone in the room of the sender but not the sender
+        cur.execute("SELECT username FROM rooms WHERE room = (?) AND isleader = (?)", (room, 1))
+        leader = cur.fetchone()
+    # Send the username of the player that just connected to everyone in the room except the new player
     emit('newjoin', username, room=room, include_self=False)
-    # Sends to only the client sender
-    emit('curusers', users, room=request.sid)
+    # Send the current users and the leader to the new player
+    for i in range(0, len(users)):
+        users[i] = users[i][0]
+    emit('curusers', (users, leader[0]), room=request.sid)
 
 
+# Event bucket for when a client disconnects
 @socketio.on('leave')
 def on_leave(data):
     username = data['username']
     room = data['room']
-    print("Player : " + username + " Left")
+    isleader = data['isleader']
+    # Make the user leave their socketio room
     leave_room(room)
+    # Remove them from the db
     with sql.connect("rooms.db") as con:
         cur = con.cursor()
         cur.execute("DELETE FROM rooms WHERE username = (?) and room = (?)", (username, room))
+        if isleader == 1:
+            cur.execute("SELECT username FROM rooms WHERE room = (?)", (room,))
+            newlead = cur.fetchone()
+            cur.execute("UPDATE rooms SET isleader = 1 WHERE username = (?) and room = (?)", (newlead[0], room))
+            emit('newleader', newlead, room=room, include_self=False)
+    # Notify everyone else in the room that user has left
     emit('newleave', username, room=room, include_self=False)
 
 
+# Function to create a join code
 def create_code():
+    # create a code
     temp = ""
     for i in range(0, 4):
         temp += str(randint(0, 9))
-
+    # check if the code already exists
     with sql.connect("rooms.db") as con:
         cur = con.cursor()
         cur.execute("SELECT room FROM rooms WHERE room = (?)", (temp,))
         temproom = cur.fetchone()
+        # if the code already exists, try again
         if temproom:
             create_code()
+        # otherwise send the code
         else:
             return temp
 
@@ -118,5 +141,5 @@ if __name__ == '__main__':
 # Initial database setup
 #   with sql.connect("rooms.db") as con:
 #        cur = con.cursor()
-#        cur.execute("CREATE TABLE rooms (username text, room integer)")
+#        cur.execute("CREATE TABLE rooms (username text, room integer, isleader BOOLEAN NOT NULL)")
 #        con.commit()
--- my_app/static/scripts/socketio.js
+++ my_app/static/scripts/socketio.js
@@ -9,43 +9,51 @@ document.addEventListener('DOMContentLoaded', () => {
     var url = new URL(queryString);
     var room = url.searchParams.get("code");
     var username = url.searchParams.get("username");
+    var Leader = "Loading..."
+
     // Event bucket for when the client connects to the server
     socket.on('connect', function() {
        // Sends a request for a room
        socket.emit('join', {'room': room, 'username': username});
     });
+    // Just before the page gets closed, notify server we left.
     window.onbeforeunload = function () {
-        socket.emit('leave', {'room': room, 'username': username});
+        if (username == Leader) {
+            socket.emit('leave', {'room': room, 'username': username, 'isleader': 1})
+        } else {
+            socket.emit('leave', {'room': room, 'username': username, 'isleader': 0});
+        }
     }
+    // Event bucket for when someone else joins the room.
     socket.on('newjoin', data => {
         console.log("New player: " + data);
         usernames.push(data);
         document.getElementById("users").innerHTML = usernames;
     });
+    // Event bucket for when someone else leaves the room.
     socket.on('newleave', data => {
         console.log("Player Left: " + data);
         var index = usernames.indexOf(data);
         if (index > -1) {
             usernames.splice(index, 1);
+            console.log("Removed player from list")
         }
         document.getElementById("users").innerHTML = usernames;
     });
-    socket.on('curusers', data => {
-        for (let key in data) {
-            for (let i in data[key]) {
-                usernames.push(data[key][i]);
-            }
+    // Event bucket for when we join the room and request the current users
+    socket.on('curusers', (users, leader) => {
+        for (let i in users) {
+            usernames.push(users[i]);
         }
-        console.log(data);
+        Leader = leader;
+        console.log(users);
+        console.log(leader);
         document.getElementById("users").innerHTML = usernames;
+        document.getElementById("leader").innerHTML = leader;
     });
-
+    // Event bucket for when a new leader is selected
+    socket.on('newleader', newleader => {
+        console.log("New leader: " + newleader);
+        document.getElementById("leader").innerHTML = newleader;
+    });
 });
-
-
-// JOIN MAPPING
-// If code exists, send room to client, client sends room back to server where the client joins the room. server sends back topic, time limit, and players (use kwargs)
-// Socket sends name to all
-
-// CREATE MAPPING
-// Send code, topic, time-limit, and username to client. Client sends back id and is added to room.
